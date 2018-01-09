d01338fadb8181fe1b57790f602afa10266c0b54
liyueliyue <1048355336@qq.com>
2018-01-09 21:07:10 +0800

    add str func

--- Chapter 2/2.1python的内置类型/2.1.1字符串与字节.py
+++ Chapter 2/2.1python的内置类型/2.1.1字符串与字节.py
@@ -16,13 +16,30 @@ print(tuple1)
 print(type(b'some bytes'))
 print(type('string'))
 
-# 字符串和字节的转换：
-# 1.str(字符串)encode()为bytes(字节)：encode()
-string = 'i am coming!'
-bytes1 = str.encode(string)
+#一、字符串和字节的转换：
+# 1.str(字符串)encode()为bytes(字节)：encode()\bytes(source,encoding='字节序列则需要指定该参数')
+string1 = 'i am coming!'
+bytes1 = str.encode(string1)
 print(bytes1)
 print(type(bytes1))
+print(bytes(string1,encoding='utf-8'))
 
-# 2.bytes(字节)decode()为str(字符串)：decode()
+# 2.bytes(字节)decode()为str(字符串)：decode()/str(source,encoding='字节序列则需要指定该参数')
 bytes2 = b'i am bytes!'
-string2 = str
+string2 = bytes.decode(bytes2)
+print(string2)
+print(str(bytes2,encoding='utf-8'))
+# 二、python字符串是不可变的，字节序列也是不可变的。
+# 三、字符串拼接
+# 1、str.join()方法接受可迭代的字符串作为参数，返回合并后的字符串
+list = ['i','am','coming','!']
+string3 = ' '.join(list)
+print(string3)
+tuple1 = ('i','am','coming','!')
+string4 = ' '.join(tuple1)
+print(string4)
+# 2、使用+拼接字符串
+a = '123'
+b = '456'
+print(a+b)
+# 3、字符串格式化可以用str.format()或%运算符
